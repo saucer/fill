39c1bff1f737cde1e157982db63fc8b481c53ef6
Curve <fynnbwdt@gmail.com>
2025-05-20 01:37:47 +0200

    fix(views): use `views` namespace

--- include/views.hpp
+++ include/views.hpp
@@ -2,7 +2,7 @@
 
 #include <range/v3/view.hpp>
 
-namespace FILL_NAMESPACE
+namespace FILL_NAMESPACE::ranges::views
 {
     inline auto join_with = ranges::views::join;
-} // namespace FILL_NAMESPACE
+} // namespace FILL_NAMESPACE::ranges::views
